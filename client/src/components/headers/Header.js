092551e91847046820859ce011f7d859f3a7b92b
shubhamHirakki <shubham@btr-india.co.in>
2022-11-13 17:50:35 +0530

    Fix: name edits

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -1,88 +1,102 @@
-import React, {useContext, useState} from 'react'
-import {GlobalState} from '../../GlobalState'
-import Menu from './icon/menu.svg'
-import Close from './icon/close.svg'
-import Cart from './icon/cart.svg'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import React, { useContext, useState } from "react";
+import { GlobalState } from "../../GlobalState";
+import Menu from "./icon/menu.svg";
+import Close from "./icon/close.svg";
+import Cart from "./icon/cart.svg";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Header() {
-    const state = useContext(GlobalState)
-    const [isLogged] = state.userAPI.isLogged
-    const [isAdmin] = state.userAPI.isAdmin
-    const [cart] = state.userAPI.cart
-    const [menu, setMenu] = useState(false)
+  const state = useContext(GlobalState);
+  const [isLogged] = state.userAPI.isLogged;
+  const [isAdmin] = state.userAPI.isAdmin;
+  const [cart] = state.userAPI.cart;
+  const [menu, setMenu] = useState(false);
 
-    const logoutUser = async () =>{
-        await axios.get('/user/logout')
-        
-        localStorage.removeItem('firstLogin')
-        
-        window.location.href = "/";
-    }
+  const logoutUser = async () => {
+    await axios.get("/user/logout");
 
-    const adminRouter = () =>{
-        return(
-            <>
-                <li><Link to="/create_product">Create Product</Link></li>
-                <li><Link to="/category">Categories</Link></li>
-            </>
-        )
-    }
+    localStorage.removeItem("firstLogin");
 
-    const loggedRouter = () =>{
-        return(
-            <>
-                <li><Link to="/history">History</Link></li>
-                <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
-            </>
-        )
-    }
+    window.location.href = "/";
+  };
 
-
-    const styleMenu = {
-        left: menu ? 0 : "-100%"
-    }
-
+  const adminRouter = () => {
     return (
-        <header>
-            <div className="menu" onClick={() => setMenu(!menu)}>
-                <img src={Menu} alt="" width="30" />
-            </div>
+      <>
+        <li>
+          <Link to="/create_product">Create Product</Link>
+        </li>
+        <li>
+          <Link to="/category">Categories</Link>
+        </li>
+      </>
+    );
+  };
 
-            <div className="logo">
-                <h1>
-                    <Link to="/">{isAdmin ? 'Admin' : 'DevAT Shop'}</Link>
-                </h1>
-            </div>
+  const loggedRouter = () => {
+    return (
+      <>
+        <li>
+          <Link to="/history">History</Link>
+        </li>
+        <li>
+          <Link to="/" onClick={logoutUser}>
+            Logout
+          </Link>
+        </li>
+      </>
+    );
+  };
 
-            <ul style={styleMenu}>
-                <li><Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link></li>
+  const styleMenu = {
+    left: menu ? 0 : "-100%",
+  };
 
-                {isAdmin && adminRouter()}
+  return (
+    <header>
+      <div className="menu" onClick={() => setMenu(!menu)}>
+        <img src={Menu} alt="" width="30" />
+      </div>
 
-                {
-                    isLogged ? loggedRouter() : <li><Link to="/login">Login ✥ Register</Link></li>
-                }
+      <div className="logo">
+        <h1>
+          <Link to="/">{isAdmin ? "Admin" : "Refillable"}</Link>
+        </h1>
+      </div>
 
-                <li onClick={() => setMenu(!menu)}>
-                    <img src={Close} alt="" width="30" className="menu" />
-                </li>
+      <ul style={styleMenu}>
+        <li>
+          <Link to="/">{isAdmin ? "Products" : "Shop"}</Link>
+        </li>
 
-            </ul>
+        {isAdmin && adminRouter()}
 
-            {
-                isAdmin ? '' 
-                :<div className="cart-icon">
-                    <span>{cart.length}</span>
-                    <Link to="/cart">
-                        <img src={Cart} alt="" width="30" />
-                    </Link>
-                </div>
-            }
-            
-        </header>
-    )
+        {isLogged ? (
+          loggedRouter()
+        ) : (
+          <li>
+            <Link to="/login">Login ✥ Register</Link>
+          </li>
+        )}
+
+        <li onClick={() => setMenu(!menu)}>
+          <img src={Close} alt="" width="30" className="menu" />
+        </li>
+      </ul>
+
+      {isAdmin ? (
+        ""
+      ) : (
+        <div className="cart-icon">
+          <span>{cart.length}</span>
+          <Link to="/cart">
+            <img src={Cart} alt="" width="30" />
+          </Link>
+        </div>
+      )}
+    </header>
+  );
 }
 
-export default Header
+export default Header;
